9bce366670ccbc642beb6e76f07f6adae511a9f9
marizvi <202051121@iiitvadodara.ac.in>
2021-10-31 13:17:37 +0530

    meal screen added

--- lib/categoryItem.dart
+++ lib/categoryItem.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Text(title),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.withOpacity(0.5),
-              color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(12)),
+    // instead of returning a container only we are wrapping it into inwell and then returning,
+    //because container does not have ontap feature
+    return InkWell(
+      // using Inkwell because it also gives ripple effect on tap :)
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color.withOpacity(0.5),
+                color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(12)),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.cyan,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.w400))),
       home: CategoriesScreen(), // this specifies the main screen of our app
     );
   }
