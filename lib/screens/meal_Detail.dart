2fb8fc78af5add38e4738a226441290c0528e7ad
marizvi <202051121@iiitvadodara.ac.in>
2021-11-06 08:06:10 +0530

    ingredients added

--- lib/screens/meal_Detail.dart
+++ lib/screens/meal_Detail.dart
@@ -29,7 +29,27 @@ class MealDetails extends StatelessWidget {
             'Ingredients',
             style: Theme.of(context).textTheme.title,
           ),
-        )
+        ),
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10)),
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          padding: EdgeInsets.all(7),
+          height: 100,
+          width: 300,
+          child: ListView.builder(
+            itemBuilder: (ctx, index) => Card(
+              color: Colors.white10,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(selectedMeal.ingredients[index]),
+              ),
+            ),
+            itemCount: selectedMeal.ingredients.length,
+          ),
+        ),
       ]),
     );
   }
